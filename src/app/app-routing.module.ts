71e8a066df87b9aa2317402e63bc8e8d75c89673
Marco Tulio Ruiz <mruizq@unah.hn>
2022-05-29 09:39:45 -0600

    Initial Structure created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 
-const routes: Routes = [];
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+
+
+const routes: Routes = [
+  {
+    path: "",
+    component: DefaultLayoutComponent,
+    children: [
+      {
+         path: "home",
+         loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
