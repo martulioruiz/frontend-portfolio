fe2323c16f603f9b0809a7078139deef92d41827
Marco Tulio Ruiz <mruizq@unah.hn>
2022-05-29 18:51:57 -0600

    square rendered

--- src/app/home/components/scene/scene.component.ts
+++ src/app/home/components/scene/scene.component.ts
@@ -5,26 +5,58 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { interval } from 'rxjs';
 import { WebGLService } from './services/web-gl.service';
-
 @Component({
   selector: 'app-scene',
   templateUrl: './scene.component.html',
   styleUrls: ['./scene.component.css'],
 })
-
 export class SceneComponent implements OnInit, AfterViewInit {
   @ViewChild('sceneCanvas') private canvas?: ElementRef<HTMLCanvasElement>;
 
+  /**
+   * The interval of refresh rate for drawing our scene during one second of elapsed time (1000ms).
+   */
+  private _60fpsInterval = 16.666666666666666667;
+  private gl?: WebGLRenderingContext
+
   constructor(private webglService: WebGLService) {}
+
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     if (!this.canvas) {
       alert('canvas not supplied! cannot bind WebGL context!');
       return;
     }
-    this.webglService.initialiseWebGLContext(this.canvas.nativeElement);
+    this.gl = this.webglService.initialiseWebGLContext(
+      this.canvas.nativeElement
+    );
+    // Set up to draw the scene periodically.
+    const drawSceneInterval = interval(this._60fpsInterval);
+    drawSceneInterval.subscribe(() => {
+      this.drawScene();
+    });
   }
 
-  ngOnInit(): void {}
+  /**
+   * Draws the scene
+   */
+  private drawScene() {
+    // prepare the scene and update the viewport
+    this.webglService.updateViewport();
+    this.webglService.prepareScene();
+
+    // draw the scene
+    const offset = 0;
+    const vertexCount = 4;
+    if(this.gl){
+      this.gl.drawArrays(
+        this.gl.TRIANGLE_STRIP,
+        offset,
+        vertexCount
+      );
+    }
+  }
 }
---
+++ src/app/shared/GLSLConstants.ts
@@ -0,0 +1,4 @@
+export class GLSLConstants {
+  static fragmentShaderMimeType = 'x-shader/x-fragment';
+  static vertexShaderMimeType = 'x-shader/x-vertex';
+}
